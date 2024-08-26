8fb0f979caec4d465197ffe1d5f2b9f502f3efc4
Ofekitach <mr.ofekitach@gmail.com>
2024-08-26 09:09:40 +0300

    missing lib directory cause of gitignore

---
+++ lib/metadata.py
@@ -0,0 +1,23 @@
+import boto3 
+
+class AwsMetadata:
+    def __init__(self):
+        self.s3_client = boto3.client('s3')
+        self.sts_client = boto3.client('sts')
+        self.buckets = self.get_buckets()
+        self.username = self.get_username()
+
+    def get_buckets(self):
+        """Retrieve and return a list of bucket names."""
+        response = self.s3_client.list_buckets()
+        buckets = [bucket['Name'] for bucket in response['Buckets']]
+        return buckets
+
+    def get_username(self):
+        """Retrieve and return the AWS user ARN."""
+        try:
+            identity = self.sts_client.get_caller_identity()
+            return identity['Arn']
+        except Exception as e:
+            print(f"Error fetching user ARN: {e}")
+            exit()
